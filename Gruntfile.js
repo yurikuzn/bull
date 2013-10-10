1a389aa697dd749d2608512efba918cdf0223b8f
Yuri Kuznetsov <yurak@list.ru>
2013-10-10 12:33:42 +0300

    grunt changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,36 +2,25 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		concat: {
-			dist: {
-				src: [
-					'src/bull.factory.js',
-					'src/bull.view.js',
-					'src/bull.loader.js',
-					'src/bull.cacher.js',
-					'src/bull.templator.js',
-					'src/bull.layouter.js',
-					'src/bull.renderer.js',
-				],
-				dest: 'build/<%= pkg.name %>.concat.js',
-			},
-		},
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
-			build: {
-				src: 'build/<%= pkg.name %>.concat.js',
-				dest: 'build/<%= pkg.name %>.min.js'
-			}
+			'build/<%= pkg.name %>.min.js': [
+				'src/bull.factory.js',
+				'src/bull.view.js',
+				'src/bull.loader.js',
+				'src/bull.cacher.js',
+				'src/bull.templator.js',
+				'src/bull.layouter.js',
+				'src/bull.renderer.js',
+			]
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	
+
 	grunt.registerTask('default', [
-		'concat',
 		'uglify'
 	]);
 };
